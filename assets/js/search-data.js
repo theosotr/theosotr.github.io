975b14dd088670497ebd2e98be93c7142bcce251
theosotr <theosotr@windowslive.com>
2025-06-22 20:56:14 +0000

    Deploying to gh-pages from @ theosotr/theosotr.github.io@8eefa5b6bd106752adfd8306fb5bb4eed860f7a8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -7,14 +7,14 @@ ninja.data = [{
     title: "about",
     section: "Navigation",
     handler: () => {
-      window.location.href = "/theosotr.github.io/";
+      window.location.href = "/";
     },
   },{id: "nav-publications",
           title: "publications",
           description: "publications in reversed chronological order.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/theosotr.github.io/publications/";
+            window.location.href = "/publications/";
           },
         },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
@@ -102,7 +102,7 @@ ninja.data = [{
         title: 'RSS Feed',
         section: 'Socials',
         handler: () => {
-          window.open("theosotr.github.io/feed.xml", "_blank");
+          window.open("/feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
